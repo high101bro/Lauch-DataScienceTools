459e7ef5f94c6a7d104fa908eb4d325322b520b0
Daniel Komnick <daniel.s.komnick.mil@army.mil>
2022-07-19 14:05:25 -0400

    dan fixed this

--- dstools/launcher.py
+++ dstools/launcher.py
@@ -1,11 +1,21 @@
-import docker
+#!/usr/bin/env python3
+import docker, webbrowser
 
-def launch_tool(t="afcai2c/jlab-cv",p=8888):
-    client = docker.from_env()
+client = docker.from_env()
+
+localhost = "http://localhost"
+image     = "afcai2c/jlab-cv"
+tag       = "latest"
+port      = 8888
+def startTool(image,tag,port):
+    imageName = "{0}:{1}".format(image,tag)
     print('after the container starts')
-    tool = client.containers.run(t,
-        ports={p:p},
+    tool = client.containers.run(
+        imageName,
+        ports={port:port},
         detach=True)
     return(tool)
-    
 
+startTool(image,tag,port)
+launchUrl = "{0}:{1}".format(localhost,port)
+webbrowser.open(launchUrl)
